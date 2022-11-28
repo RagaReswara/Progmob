ac332a791054b324eae407fa6a29715f4a89698f
Raga Reswara <ragareswara55@gmail.com>
2022-11-28 18:54:13 +0700

    Pertemuan 13

---
+++ startup_namer/lib/home.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return
+      Scaffold(
+        appBar: AppBar(
+          title: Text("Home Drawer"),
+        ),
+        drawer: Drawer(
+          child:
+          ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              UserAccountsDrawerHeader(
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: AssetImage("images/image.jpg"),
+                    backgroundColor: Colors.green,
+                  ),
+                  accountName: Text("Jojo"),
+                  accountEmail: Text("jojo100@gmail.com")),
+
+              ListTile(
+                title: const Text('My Account'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+              ListTile(
+                title: const Text('Top-Up'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+              ListTile(
+                title: const Text('Settings'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+            ],
+          ),
+        ),
+        body: ListView(
+          children: [
+            buildContainer(),
+            buildContainer(),
+            buildContainer(),
+            buildCard()
+          ],
+        ),
+      );
+  }
+  Card buildCard(){
+       return Card(
+              elevation: 8,
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.add_to_photos_rounded),
+                    title: Text("Tiket To Mesir"),
+                    subtitle: Text("Just Need your Blood")
+                  ),
+                  Row(
+                    children: [
+                      TextButton(
+                          onPressed: (){},
+                          child: Text("Buy")),
+                      TextButton(
+                          onPressed: (){},
+                          child: Text("View")),
+                      TextButton(
+                          onPressed: (){},
+                          child: Text("Close"))
+                    ],
+                  )
+                ],
+              ),
+            );
+  }
+      Container buildContainer() {
+        return Container(
+          decoration: BoxDecoration(
+            border: Border.all(width: 5
+            ),
+          ),
+          child: Text("Mitologi Mesir adalah himpunan mitos-mitos dari Mesir kuno, yang melukiskan tindakan-tindakan para dewa Mesir sebagai sarana untuk memahami dunia. Keyakinan-keyakinan yang terkandung dalam mitos-mitos ini merupakan bagian penting dalam agama Mesir kuno. Mitos-mitos sering muncul dalam sastra dan Seni Mesir Kuno, terutama dalam cerita-cerita pendek dan materi keagamaan seperti nyanyian pujian, teks ritual, teks pemakaman, dan dekorasi kuil. Sumber-sumber ini jarang berisi versi lengkap sebuah mitos dan kebanyakan hanya mengandung bagian penggambaran singkatnya saja."),
+          padding: EdgeInsets.all(10.0),
+        );
+      }
+}
--- startup_namer/lib/pertemuan9.dart
+++ startup_namer/lib/pertemuan9.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:startup_namer/main.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,6 +23,7 @@ class MyApp extends StatelessWidget {
 }
 
 class pertemuan9 extends StatefulWidget {
+  //pertemuan9(Key key, this.title) : super(key:key);
   const pertemuan9({super.key, required this.title});
 
   final String title;
@@ -30,6 +33,7 @@ class pertemuan9 extends StatefulWidget {
 }
 
 class _pertemuan9State extends State<pertemuan9> {
+
   int _counter = 0;
 
   void _incrementCounter() {
@@ -50,8 +54,11 @@ class _pertemuan9State extends State<pertemuan9> {
           children: <Widget>[
             TextFormField(
               decoration: new InputDecoration(
-                labelText: "Tex Input",
-                hintText: "Teks yang akan diinput formatnya adalah sbb",
+                labelText: "Email or Username",
+                hintText: "Input at least 8-15 Letters",
+                  border: OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(5)
+                  )
               ),
             ),
             Padding(
@@ -59,19 +66,35 @@ class _pertemuan9State extends State<pertemuan9> {
             ),
             TextFormField(
               decoration: new InputDecoration(
-                labelText: "Tex Input 2",
-                hintText: "Teks yang akan diinput formatnya adalah sbb",
+                labelText: "Password",
+                hintText: "Need 3 different number",
                 border: OutlineInputBorder(
                   borderRadius: new BorderRadius.circular(5)
                 )
               ),
             ),
-            ElevatedButton(onPressed: (){}, child: Text(
+            ElevatedButton(onPressed: (){
+
+            }, child: Text(
               "Submit",
               style: TextStyle(
                 color: Colors.white
               ),
+            )),
+
+            ElevatedButton(onPressed: () async {
+              SharedPreferences pref = await SharedPreferences.getInstance();
+              await pref.setInt("is_login", 0);
+              Navigator.pushReplacement(
+                  context,
+              MaterialPageRoute(builder: (context) => MyHomePage(title: "Halo"));
+            }, child: Text(
+              "Logout",
+              style: TextStyle(
+                  color: Colors.white
+              ),
             ))
+
           ],
         ),
       ),
